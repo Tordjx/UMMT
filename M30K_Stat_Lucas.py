19b024ad9d1ea91a8a3d8ec8e9e1d9b557e687d7
lucasdegeorge <103430109+lucasdegeorge@users.noreply.github.com>
2022-12-02 13:49:27 +0100

    Update M30K_Stat_Lucas.py

--- M30K_Stat_Lucas.py
+++ M30K_Stat_Lucas.py
@@ -39,7 +39,11 @@ for sentence in list_fr:
 
 
 def dict_normalization(d):
-    factor=1.0/sum(d.values())
+    # print(d)
+    try:
+        factor=1.0/sum(d.values())
+    except:
+        print(d)
     res = {}
     for k in d:
         res[k] = d[k]*factor
@@ -54,7 +58,7 @@ def histogram(list_dict,legend):
 
 ## Plotting 
 
-histogram([letters_en, letters_en],["Letters in Frnech","Letters in English"])
+# histogram([letters_en, letters_en],["Letters in Frnech","Letters in English"])
 # Does it work well ? 
 
 #%% Pairs of letters
@@ -72,6 +76,11 @@ for sentence in list_fr:
             pairs_fr[sentence[i]] = {sentence[i+1]:1}
 
 
+# pairs_fr = {key: value for key, value in sorted(pairs_fr.items()) if key.isalpha()}
+# for x in pairs_fr.keys():
+#     pairs_fr[x] = dict_normalization({key: value for key, value in sorted(pairs_fr[x].items()) if key.isalpha()})
+
+
 pairs_en = {}
 
 for sentence in list_en:
@@ -82,14 +91,46 @@ for sentence in list_en:
             else: 
                 pairs_en[sentence[i]][sentence[i+1]]=1
         else: 
-            pairs_en[sentence[i]] = {sentence[i+1]:1}
+            pairs_en[sentence[i]] = {sentence[i+1] : 1}
+
+pairs_en = {key: value for key, value in sorted(pairs_en.items()) if key.isalpha()}
+for x in pairs_en.keys():
+    pairs_en[x] = dict_normalization({key: value for key, value in sorted(pairs_en[x].items()) if key.isalpha()})
+
+
 
 #%% Matrix of following letter
 
-n_en = len(pairs_en)
-n_fr = len(pairs_fr)
+list_letters_en = sorted([ key for key in pairs_en.keys() ])
+list_letters_fr = sorted([ key for key in pairs_fr.keys() ])
 
-followers_en = np.zeros(n_en)
-followers_fr = np.zeros(n_fr)
+n_en = len(list_letters_en)
+n_fr = len(list_letters_fr)
+
+followers_en = np.zeros((n_en,n_en),dtype=np.float64)
+followers_fr = np.zeros((n_fr,n_fr),dtype=np.float64)
+
+for i in range(len(list_letters_en)):
+    for j in range(len(list_letters_en)):
+        try:
+            followers_en[i,j] = pairs_fr[list_letters_en[i]].get(list_letters_en[j]) # / letters_en.get(list_letters_en[i])
+        except:
+            followers_en[i,j] = 0
+
+
+
+# for i in range(len(list_letters_fr)):
+#     for j in range(len(list_letters_fr)):
+#         try:
+#             followers_fr[i,j] = pairs_fr[list_letters_fr[i]].get(list_letters_fr[j]) # / letters_fr.get(list_letters_fr[i])
+#         except:
+#             followers_fr[i,j] = 0
+
+plt.figure()
+plt.pcolormesh(followers_en)
+plt.colorbar()
+plt.show()
+
+#%% Lenght of the words, the sentences
 
 

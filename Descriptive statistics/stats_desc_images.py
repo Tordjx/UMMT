aaa0bb6f7b81b189074a262b65f29873dbb228c6
lucasdegeorge <103430109+lucasdegeorge@users.noreply.github.com>
2023-03-08 10:00:30 +0100

    Stats desc clip

--- Descriptive statistics/stats_desc_images.py
+++ Descriptive statistics/stats_desc_images.py
@@ -4,98 +4,63 @@ import clip
 from PIL import Image
 import os
 from torchvision.datasets import CIFAR100
-
-
-device = "cuda" if torch.cuda.is_available() else "cpu"
-
 import numpy as np
 import matplotlib.pyplot as plt
-
-
-
-#%% loading data
-
-images = np.load("C:/Users/lucas/Desktop/train-resnet50-res4frelu.npy")
-
-#%% CLIP model example
-
-model, preprocess = clip.load("ViT-B/32", device=device)
-
-image = preprocess(Image.open("C:/Users/lucas/Documents/GitHub/UMMT/CLIP.png")).unsqueeze(0).to(device)
-text = clip.tokenize(["a diagram", "a dog", "a cat"]).to(device)
-
-with torch.no_grad():
-    image_features = model.encode_image(image)
-    text_features = model.encode_text(text)
-    
-    logits_per_image, logits_per_text = model(image, text)
-    probs = logits_per_image.softmax(dim=-1).cpu().numpy()
-
-print("Label probs:", probs)  # prints: [[0.9927937  0.00421068 0.00299572]]
-
-
-#%% Get text 
-
-def get_vocab() : 
-    fichier_vocab_fr = open('C:/Users/lucas/Documents/GitHub/UMMT/vocab.fr')
-    fichier_vocab_en = open('C:/Users/lucas/Documents/GitHub/UMMT/vocab.en')
-    vocab_en = [line.split()[0] for line in fichier_vocab_en if len(line.split()) == 2]
-    vocab_en = dict((y,x) for (x,y) in enumerate(vocab_en))
-    vocab_fr = [line.split()[0] for line in fichier_vocab_fr if len(line.split()) == 2]
-    vocab_fr = dict((y,x) for (x,y) in enumerate(vocab_fr))
-    fichier_vocab_en.close()
-    fichier_vocab_fr.close()
-    return vocab_en, vocab_fr
-
-
-def get_train_data():
-    vocab_en,vocab_fr = get_vocab()
-    fichier_train_fr = open('C:/Users/lucas/Documents/GitHub/UMMT/train.BPE.fr')
-    fichier_train_en = open('C:/Users/lucas/Documents/GitHub/UMMT/train.BPE.en')
-    train_data_fr = [ligne.strip().split(" ") for ligne in fichier_train_fr ]
-    train_data_en = [ligne.strip().split(" ") for ligne in fichier_train_en ]
-    fichier_train_en.close()
-    fichier_train_fr.close()
-
-    for ligne in train_data_en: 
-        for mot in ligne : 
-            if mot not in vocab_en: 
-                vocab_en[mot] = len(vocab_en.keys())
-
-    for ligne in train_data_fr: 
-        for mot in ligne: 
-            if mot not in vocab_fr: 
-                vocab_fr[mot] = len(vocab_fr.keys())
-
-    embedded_fr = [torch.tensor([vocab_fr[x]  for x in ligne ], dtype= torch.long) for ligne in train_data_fr]
-    embedded_en = [torch.tensor([vocab_en[x]  for x in ligne ], dtype= torch.long) for ligne in train_data_en]
-
-    sentences_en = [ embedded_en[i].view(len(embedded_en[i]),1) for i in range(len(embedded_en)) ]
-    sentences_fr = [ embedded_fr[i].view(len(embedded_fr[i]),1) for i in range(len(embedded_fr)) ]
-
-    # sentences_en = torch.cat(sentences_en)
-    # sentences_fr = torch.cat(sentences_fr)
-
-    return sentences_en, sentences_fr
-
-
-
-#%% Similarity computation 
+from PIL import Image
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
-# Model 
+#%% Get correspondance between images and captions
+
+def get_captions():
+    # file names
+    file = open("C:/Users/lucas/Documents/GitHub/dataset/data/task1/image_splits/train.txt")
+    names = [line[:-1] for line in file ]
+    file.close()
+    # Captions
+    file = open("C:/Users/lucas/Desktop/train.en")
+    captions = [line[:-1] for line in file ]
+    file.close()
+    return names, captions
+
+names, captions = get_captions()
+dict_captions = { names[i] : captions[i] for i in range(len(names)) }
+
+#%% Similarity computation
+
 model, preprocess = clip.load('ViT-B/32', device)
 
-# Data 
-# images = np.load("C:/Users/lucas/Desktop/train-resnet50-res4frelu.npy")
-text_en, text_fr = get_train_data()
-print(text_en[34].shape)
+def similarity(file_name):
+    # image 
+    im = Image.open(r"C:/Users/lucas/Downloads/flickr30k-images.tar/flickr30k-images/"+ file_name) 
+    image = preprocess(im).unsqueeze(0).to(device)
+    # text 
+    texts = clip.tokenize(names).to(device)
+    
+    with torch.no_grad():
+        image_features = model.encode_image(image)
+        text_features = model.encode_text(texts)
+
+    image_features /= image_features.norm(dim=-1, keepdim=True)
+    text_features /= text_features.norm(dim=-1, keepdim=True)
+    similarity = (100.0 * image_features @ text_features.T).softmax(dim=-1)
+
+    values, indices = similarity[0].topk(5)
+
+    # Print the result
+    print("\nTop predictions:\n")
+    for value, index in zip(values, indices):
+        print(value + " " + index)
+
+similarity(names[0])
 
 
+#%% Test tuto 
 
-#%% 
+device = "cuda" if torch.cuda.is_available() else "cpu"
+model, preprocess = clip.load('ViT-B/32', device)
 
+# Download the dataset
 cifar100 = CIFAR100(root=os.path.expanduser("~/.cache"), download=True, train=False)
 
 # Prepare the inputs
@@ -103,11 +68,3 @@ image, class_id = cifar100[3637]
 image_input = preprocess(image).unsqueeze(0).to(device)
 text_inputs = torch.cat([clip.tokenize(f"a photo of a {c}") for c in cifar100.classes]).to(device)
 
-# Calculate features
-with torch.no_grad():
-    image_features = model.encode_image(image_input)
-    text_features = model.encode_text(text_inputs)
-
-#%% 
-
-T = torch.Tensor([ [text_en[i][j] for j in range(len(text_en[i]))] for i in range(len(text_en)) ] )

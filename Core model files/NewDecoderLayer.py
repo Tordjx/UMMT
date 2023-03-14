f46aa8feb708db798a434cf2263e76a90249cb6b
lucasdegeorge <103430109+lucasdegeorge@users.noreply.github.com>
2023-03-14 09:13:15 +0100

    batch_size added + padding mask dimensions issues fixed

--- Core model files/NewDecoderLayer.py
+++ Core model files/NewDecoderLayer.py
@@ -12,7 +12,7 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
 class TransformerDecoderLayer(nn.Module):
-    def __init__(self, d_model, heads,dim_feedforward, dropout=0.1,device=device):
+    def __init__(self, d_model, heads,dim_feedforward, dropout=0.1,device=device, batch_first = True):
         super().__init__()
         self.norm_1 = nn.LayerNorm(d_model)
         self.norm_2 = nn.LayerNorm(d_model)
@@ -22,8 +22,8 @@ class TransformerDecoderLayer(nn.Module):
         self.dropout_2 = nn.Dropout(dropout)
         self.dropout_3 = nn.Dropout(dropout)
         
-        self.attn_1 = nn.MultiheadAttention(d_model,heads,dropout)
-        self.attn_2 = MultiModalAttention(d_model, heads, dropout, lambda1=1, lambda2=1, device=device) # our own multiheadattention layer 
+        self.attn_1 = nn.MultiheadAttention(d_model,heads,dropout, batch_first=batch_first)
+        self.attn_2 = MultiModalAttention(d_model, heads, dropout, lambda1=1, lambda2=1, device=device, batch_first=batch_first) # our own multiheadattention layer 
         self.ffn = nn.Sequential(nn.Linear(d_model,dim_feedforward),
             nn.ReLU(),
             nn.Dropout(dropout),
@@ -37,7 +37,7 @@ class TransformerDecoderLayer(nn.Module):
             i_outputs = x[1]
             x2 = self.norm_1(text)
             x=text
-            x = x + self.dropout_1(self.attn_1(x2, x2, x2, tgt_mask, tgt_key_padding_mask)[0])
+            x = x + self.dropout_1(self.attn_1(x2, x2, x2, key_padding_mask=tgt_key_padding_mask, attn_mask=tgt_mask)[0])
             # Here, att1 returns a tuple, the first being the result, the second being the attention weights
             x2 = self.norm_2(x)
             ei_outputs = torch.cat((memory, i_outputs), 1)
@@ -49,7 +49,7 @@ class TransformerDecoderLayer(nn.Module):
             text = x
             i_outputs = None
             x2 = self.norm_1(text)
-            x = x + self.dropout_1(self.attn_1(x2, x2, x2, tgt_mask)[0])
+            x = x + self.dropout_1(self.attn_1(x2, x2, x2, key_padding_mask=tgt_key_padding_mask, attn_mask=tgt_mask)[0])
             # Here, att1 returns a tuple, the first being the result, the second being the attention weights
             x2 = self.norm_2(x)
             ei_outputs = None

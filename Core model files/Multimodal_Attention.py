72a58215b4810f8897e72c7493af9768fdfd8529
lucasdegeorge <103430109+lucasdegeorge@users.noreply.github.com>
2023-03-14 09:12:41 +0100

    batch_size argument added

--- Core model files/Multimodal_Attention.py
+++ Core model files/Multimodal_Attention.py
@@ -33,11 +33,12 @@ def attention(q, k, v, d_k, mask=None, padding_mask=None, dropout=None):
 #%% Multimodal Attention
 
 class MultiModalAttention(nn.Module):
-    def __init__(self, d_model, n_heads, dropout, lambda1=1, lambda2=1, device=device):
+    def __init__(self, d_model, n_heads, dropout, lambda1=1, lambda2=1, device=device, batch_first=True):
         super().__init__()
         self.d_model = d_model
         self.d_k = d_model // n_heads
         self.h = n_heads
+        self.batch_first = batch_first
 
         self.lambda1 = lambda1
         self.lambda2 = lambda2
@@ -58,45 +59,48 @@ class MultiModalAttention(nn.Module):
 
 
     def forward(self, q, k_e, k_i, k_ei, v_e, v_i, v_ei, mask, padding_mask, image_bool):
-        bs = q.size(1)
-
-        q = self.q_linear(q).view(-1, q.size(1), self.h, self.d_k) 
-        q = q.transpose(1,2)
-
-        # Matrices for text
-        k_e = self.k_e_linear(k_e).view(-1, k_e.size(1), self.h, self.d_k) 
-        k_e = k_e.transpose(1,2)
-        v_e = self.v_e_linear(v_e).view(-1, v_e.size(1), self.h, self.d_k)
-        v_e = v_e.transpose(1,2)
-        scores_e = attention(q, k_e, v_e, self.d_k, mask, padding_mask, self.dropout) 
-
-        # If there is only text in the input, image_bool = False
-        if not(image_bool):
-            concat = scores_e.transpose(1,2).contiguous().view(-1, bs, self.d_model)
-            output = self.out(concat)
-            return output
+        if not(self.batch_first):
+            raise TypeError("The dimensions of the inputs are not batch_size * seq_len * embedding")
         else:
-            bs_i = k_i.size(0)
-            # Score for image : 
-            k_i = self.k_i_linear(k_i).view(-1, k_i.size(1), self.h, self.d_k) 
-            k_i = k_i.transpose(1,2)
-            v_i = self.v_i_linear(v_i).view(-1, v_i.size(1), self.h, self.d_k)
-            v_i = v_i.transpose(1,2)
-            scores_i = attention(q, k_i, v_i, self.d_k, mask, padding_mask, self.dropout)
+            bs = q.size(1)
 
-            # Score for text and image : 
-            k_ei = self.k_ei_linear(k_ei).view(-1, k_ei.size(1), self.h, self.d_k) 
-            k_ei = k_ei.transpose(1,2)
-            v_ei = self.v_ei_linear(v_ei).view(-1, v_ei.size(1), self.h, self.d_k)
-            v_ei = v_ei.transpose(1,2)
-            scores_ei = attention(q, k_ei, v_ei, self.d_k, mask, padding_mask, self.dropout)
+            q = self.q_linear(q).view(-1, q.size(1), self.h, self.d_k) 
+            q = q.transpose(1,2)
 
-            # final scores 
-            scores = scores_e + self.lambda1 * scores_i + self.lambda2 * scores_ei
-            concat = scores.transpose(1,2).contiguous().view(-1, bs, self.d_model)
-            output = self.out(concat)
+            # Matrices for text
+            k_e = self.k_e_linear(k_e).view(-1, k_e.size(1), self.h, self.d_k) 
+            k_e = k_e.transpose(1,2)
+            v_e = self.v_e_linear(v_e).view(-1, v_e.size(1), self.h, self.d_k)
+            v_e = v_e.transpose(1,2)
+            scores_e = attention(q, k_e, v_e, self.d_k, mask, padding_mask, self.dropout) 
 
-            return output
+            # If there is only text in the input, image_bool = False
+            if not(image_bool):
+                concat = scores_e.transpose(1,2).contiguous().view(-1, bs, self.d_model)
+                output = self.out(concat)
+                return output
+            else:
+                bs_i = k_i.size(0)
+                # Score for image : 
+                k_i = self.k_i_linear(k_i).view(-1, k_i.size(1), self.h, self.d_k) 
+                k_i = k_i.transpose(1,2)
+                v_i = self.v_i_linear(v_i).view(-1, v_i.size(1), self.h, self.d_k)
+                v_i = v_i.transpose(1,2)
+                scores_i = attention(q, k_i, v_i, self.d_k, mask, padding_mask, self.dropout)
+
+                # Score for text and image : 
+                k_ei = self.k_ei_linear(k_ei).view(-1, k_ei.size(1), self.h, self.d_k) 
+                k_ei = k_ei.transpose(1,2)
+                v_ei = self.v_ei_linear(v_ei).view(-1, v_ei.size(1), self.h, self.d_k)
+                v_ei = v_ei.transpose(1,2)
+                scores_ei = attention(q, k_ei, v_ei, self.d_k, mask, padding_mask, self.dropout)
+
+                # final scores 
+                scores = scores_e + self.lambda1 * scores_i + self.lambda2 * scores_ei
+                concat = scores.transpose(1,2).contiguous().view(-1, bs, self.d_model)
+                output = self.out(concat)
+
+                return output
         
 
 # Old version

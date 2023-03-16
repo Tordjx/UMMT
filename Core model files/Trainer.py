6ec3bab9426955cb07091595e388eb3059ab1406
Tordjx <valentin.tordjman--levavasseur@ensae.fr>
2023-03-16 20:17:34 +0100

    added continuous cycle consistency

--- Core model files/Trainer.py
+++ Core model files/Trainer.py
@@ -7,54 +7,11 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 bptt = 10
 epoch = 1
 torch.autograd.set_detect_anomaly(True)
-def train_auto_encoding(model,train_data):
-
-    loss_list=[]
-    model.train()  # turn on train mode
-    total_loss = 0.
-    log_interval = 10
-    start_time = time.time()
-    src_mask = model.generate_square_subsequent_mask(bptt).to(device)
-    num_batches = len(train_data) 
-    for i in range (len(train_data)):
-
-        # data, target = get_batch(train_data, i,device)
-        data= batchify(train_data[i],device,10)
-        target = train_data[i]
-        seq_len = data.size(0)
-        if seq_len != bptt:  # only on last batch
-            src_mask = src_mask[:seq_len, :seq_len]
-        # print(data.device,target.device,  src_mask.device)
-        output = model(data)
-        loss = model.criterion(output.view(-1, model.n_token),target)
-
-        model.optimizer.zero_grad()
-        loss.backward()
-        torch.nn.utils.clip_grad_norm_(model.parameters(), 0.5)
-        model.optimizer.step()
-        total_loss += loss.item()
-        loss_list.append(loss.item())
-        
-        if i % log_interval == 0 and i > 0:
-            lr = model.scheduler.get_last_lr()[0]
-            ms_per_batch = (time.time() - start_time) * 1000 / log_interval
-            cur_loss = total_loss / log_interval
-            ppl = np.exp(cur_loss)
-            print(f'| epoch {epoch:3d} | {i:5d}/{num_batches:5d} batches | '
-                    f'lr {lr:02.2f} | ms/batch {ms_per_batch:5.2f} | '
-                    f'loss {cur_loss:5.2f} | ppl {ppl:8.2f}')
-            total_loss = 0
-            start_time = time.time()
-    return loss_list
-
 def auto_encoding_train(model,train_data, image_bool):
-
     if image_bool : 
         data, feature = train_data
     else : 
         data,target = train_data
-    # data, target = get_batch(train_data, i,device)
-    # print(data.device,target.device,  src_mask.device)
     if image_bool : 
         output = model(data,True,feature)
         loss = model.criterion(output.mT,data)
@@ -67,98 +24,110 @@ def auto_encoding_train(model,train_data, image_bool):
     model.optimizer.step()
     return loss.item()
 
-
 def cycle_consistent_forward(model_A,model_B,text_input, image_input = None, image_bool = False) : 
-    # Encode Text
-    
     src_mask = model_A.generate_square_subsequent_mask(model_A.n_head*text_input.shape[0],text_input.shape[1]) # square mask 
     tgt_mask = model_A.generate_square_subsequent_mask(model_A.n_head*text_input.shape[0],text_input.shape[1])
     src_padding_mask  = (text_input== 6574).to(device=device)
     tgt_padding_mask = (text_input== 6574).to(device=device)
-    # src_padding_mask = None
-    # tgt_padding_mask=None
     memory_mask = model_A.generate_square_subsequent_mask(text_input.shape[0],text_input.shape[1])
     memory_key_padding_mask = (text_input == 6574).to(device=device)
     if image_bool:
         mem_ei_mask = torch.zeros([text_input.shape[0], text_input.shape[1], text_input.shape[1] + image_input.shape[1]]).to(device=device)
-        # mem_ei_mask = torch.zeros([text_input.shape[0], text_input.shape[1] + image_input.shape[1], text_input.shape[1] + image_input.shape[1]])  # Other dimension for the mem_ei_mask for test
         mem_ei_mask[:,0:text_input.shape[1], 0:text_input.shape[1]] = model_A.generate_square_subsequent_mask(text_input.shape[0],text_input.shape[1]).to(device=device)
         mem_ei_key_padding_mask = (text_input == 6574).to(device=device)
         mem_ei_key_padding_mask = torch.cat((mem_ei_key_padding_mask, torch.full([text_input.shape[0], image_input.shape[1]], False).to(device=device)), dim=1)
-    
     text_encoded = model_A.encoder(model_A.positional_encoder(model_A.embedding(text_input)),src_mask,src_padding_mask)
-    # print("Text encoded : ")
-    # print(text_encoded)
     if image_bool:
-        # Concatenate encoded text and image
         mem_masks = [memory_mask, mem_ei_mask]
         mem_padding_masks = [memory_key_padding_mask, mem_ei_key_padding_mask]
         image_encoded = model_A.feedforward(image_input)
         x = [text_encoded, image_encoded]
         output = model_B.decoder(x,model_A.positional_encoder(model_A.embedding(text_input)), tgt_mask , mem_masks , tgt_padding_mask, mem_padding_masks)
-        # print("output decoder")
-        # print(output)
     else:
         x = text_encoded
-    # Pass through the decoder
         output = model_B.decoder(x,model_A.positional_encoder(model_A.embedding(text_input)), tgt_mask , [memory_mask] , tgt_padding_mask, [memory_key_padding_mask])
-    # print("output softamxé")
-    # print(model_B.activation(model_B.output_layer(output)))
-
     return model_B.activation(model_B.output_layer(output))
 
+def differentiable_cycle_forward(model_A,model_B,text_input, image_input = None, image_bool = False):
+    src_mask = model_A.generate_square_subsequent_mask(model_A.n_head*text_input.shape[0],text_input.shape[1]) # square mask 
+    tgt_mask = model_A.generate_square_subsequent_mask(model_A.n_head*text_input.shape[0],text_input.shape[1])
+    src_padding_mask  = (text_input== 6574).to(device=device)
+    tgt_padding_mask = (text_input== 6574).to(device=device)
+    memory_mask = model_A.generate_square_subsequent_mask(text_input.shape[0],text_input.shape[1])
+    memory_key_padding_mask = (text_input == 6574).to(device=device)
+    if image_bool:
+        mem_ei_mask = torch.zeros([text_input.shape[0], text_input.shape[1], text_input.shape[1] + image_input.shape[1]]).to(device=device)
+        mem_ei_mask[:,0:text_input.shape[1], 0:text_input.shape[1]] = model_A.generate_square_subsequent_mask(text_input.shape[0],text_input.shape[1]).to(device=device)
+        mem_ei_key_padding_mask = (text_input == 6574).to(device=device)
+        mem_ei_key_padding_mask = torch.cat((mem_ei_key_padding_mask, torch.full([text_input.shape[0], image_input.shape[1]], False).to(device=device)), dim=1)
+    text_encoded = model_A.encoder(model_A.positional_encoder(model_A.embedding(text_input)),src_mask,src_padding_mask)
+    if image_bool:
+        mem_masks = [memory_mask, mem_ei_mask]
+        mem_padding_masks = [memory_key_padding_mask, mem_ei_key_padding_mask]
+        image_encoded = model_A.feedforward(image_input)
+        x = [text_encoded, image_encoded]
+        output = model_B.decoder(x,model_A.positional_encoder(model_A.embedding(text_input)), tgt_mask , mem_masks , tgt_padding_mask, mem_padding_masks)
+    else:
+        x = text_encoded
+        output = model_B.decoder(x,model_A.positional_encoder(model_A.embedding(text_input)), tgt_mask , [memory_mask] , tgt_padding_mask, [memory_key_padding_mask])
+    #Intermediate result to have the new masks
+    with torch.no_grad():
+        text_input = torch.argmax(model_B.activation(model_B.output_layer(output)),dim = 2)
+    #Compute new masks with augmented data
+    src_mask = model_A.generate_square_subsequent_mask(model_A.n_head*text_input.shape[0],text_input.shape[1]) # square mask 
+    tgt_mask = model_A.generate_square_subsequent_mask(model_A.n_head*text_input.shape[0],text_input.shape[1])
+    src_padding_mask  = (text_input== 6574).to(device=device)
+    tgt_padding_mask = (text_input== 6574).to(device=device)
+    memory_mask = model_A.generate_square_subsequent_mask(text_input.shape[0],text_input.shape[1])
+    memory_key_padding_mask = (text_input == 6574).to(device=device)
+    if image_bool:
+        mem_ei_mask = torch.zeros([text_input.shape[0], text_input.shape[1], text_input.shape[1] + image_input.shape[1]]).to(device=device)
+        mem_ei_mask[:,0:text_input.shape[1], 0:text_input.shape[1]] = model_A.generate_square_subsequent_mask(text_input.shape[0],text_input.shape[1]).to(device=device)
+        mem_ei_key_padding_mask = (text_input == 6574).to(device=device)
+        mem_ei_key_padding_mask = torch.cat((mem_ei_key_padding_mask, torch.full([text_input.shape[0], image_input.shape[1]], False).to(device=device)), dim=1)
+    text_encoded = model_A.encoder(model_A.positional_encoder(output),src_mask,src_padding_mask)
+    if image_bool:
+        mem_masks = [memory_mask, mem_ei_mask]
+        mem_padding_masks = [memory_key_padding_mask, mem_ei_key_padding_mask]
+        image_encoded = model_A.feedforward(image_input)
+        x = [text_encoded, image_encoded]
+        output = model_B.decoder(x,model_A.positional_encoder(model_A.embedding(text_input)), tgt_mask , mem_masks , tgt_padding_mask, mem_padding_masks)
+    else:
+        x = text_encoded
+        output = model_B.decoder(x,model_A.positional_encoder(model_A.embedding(text_input)), tgt_mask , [memory_mask] , tgt_padding_mask, [memory_key_padding_mask])
+    return model_B.activation(model_B.output_layer(output))
 
-# Old version 
-# def cycle_consistent_forward(model_A,model_B,text_input, image_input = None, image_bool = False) : 
-#     # Encode Text
-    
-#     src_mask = model_A.generate_square_subsequent_mask(model_A.n_head*text_input.shape[0],text_input.shape[1]) # square mask 
-#     tgt_mask = model_A.generate_square_subsequent_mask(model_A.n_head*text_input.shape[0],text_input.shape[1])
-#     src_padding_mask  = (text_input== 6574).to(device=device)
-#     tgt_padding_mask = (text_input== 6574).to(device=device)
-#     # src_padding_mask = None
-#     # tgt_padding_mask=None
-#     memory_mask = None
-#     memory_key_padding_mask =None
-#     text_encoded = model_A.encoder(model_A.positional_encoder(model_A.embedding(text_input)),src_mask,src_padding_mask)
-#     if image_bool:
-#         # Concatenate encoded text and image
-#         image_encoded =model_A.feedforward(image_input)
-#         x = [text_encoded, image_encoded]
-#     else:
-#         x = text_encoded
-#     # Pass through the decoder
-#     output = model_B.decoder(x,model_A.positional_encoder(model_A.embedding(text_input)), tgt_mask , memory_mask , tgt_padding_mask, memory_key_padding_mask)
-#     return model_B.output_layer(output)
-
-
-
-
-
-def cycle_consistency_train(model_A, model_B,train_data,image_bool=False):
-        
+def differentiable_cycle_consistency_train(model_A, model_B,train_data,image_bool=False):
         if image_bool : 
             data, features = train_data
         else :
             data,target = train_data
-
-
         if image_bool : 
-            # print("premier cycle softmaxé")
-            first_output = torch.argmax(cycle_consistent_forward(model_A,model_B, data, features, image_bool),dim = 2)
-            # print(first_output)
-            output = cycle_consistent_forward(model_B,model_A, first_output, features, image_bool)
-            # print("forward output")
-            # print(output)
+            output = differentiable_cycle_forward(model_A,model_B, data, features, image_bool)
             loss = model_A.criterion(output.mT,data)
-            # print('output')
-            # print(output)
-            # print("input data")
-            # print(data)
-            # print('loss')
-            # print(loss)
         else :
+            output = cycle_consistent_forward(model_B,model_A, torch.argmax(cycle_consistent_forward(model_A,model_B, data),dim = 2))
+            loss = model_A.criterion(output.mT,target)
+        model_A.optimizer.zero_grad()
+        model_B.optimizer.zero_grad()
+        loss.backward()
+        torch.nn.utils.clip_grad_norm_(model_A.parameters(), 0.5)
+        torch.nn.utils.clip_grad_norm_(model_B.parameters(), 0.5)    
+        model_A.optimizer.step()
+        model_B.optimizer.step()
+        return loss.item()
 
+def cycle_consistency_train(model_A, model_B,train_data,image_bool=False):
+        if image_bool : 
+            data, features = train_data
+        else :
+            data,target = train_data
+        if image_bool : 
+            with torch.no_grad():
+                first_output = torch.argmax(cycle_consistent_forward(model_A,model_B, data, features, image_bool),dim = 2)
+            output = cycle_consistent_forward(model_B,model_A, first_output, features, image_bool)
+            loss = model_A.criterion(output.mT,data)
+        else :
             output = cycle_consistent_forward(model_B,model_A, torch.argmax(cycle_consistent_forward(model_A,model_B, data),dim = 2))
             loss = model_A.criterion(output.mT,target)
         model_A.optimizer.zero_grad()
@@ -166,13 +135,11 @@ def cycle_consistency_train(model_A, model_B,train_data,image_bool=False):
         loss.backward()
         torch.nn.utils.clip_grad_norm_(model_A.parameters(), 0.5)
         torch.nn.utils.clip_grad_norm_(model_B.parameters(), 0.5)
-        
         model_A.optimizer.step()
         model_B.optimizer.step()
         return loss.item()
 
 import matplotlib.pyplot as plt
-proba = 1
 def mixed_train(model_fr,model_en,train_data_fr,train_data_en,n_iter,batch_size, image_bool = False):
     loss_list = []
     model_fr.train()
@@ -186,50 +153,31 @@ def mixed_train(model_fr,model_en,train_data_fr,train_data_en,n_iter,batch_size,
         else : 
             N = len(train_data_fr[0])
         for i in range(N):
-            if np.random.rand()<proba : #Cycle consistency
-                
-                if np.random.rand()<1/2 : 
-                    # print("données EN")
-                    if image_bool : 
-                        train_data= get_batch(train_data_en,i,image_bool)
-                        
-                    else : 
-                        train_data= get_batch(train_data_en,i)
-                    model_A = model_en
-                    model_B = model_fr
+            U = np.random.rand()
+            V = np.random.rand()
+            if U<1/2 : #ENGLISH DATA
+                if image_bool : 
+                    train_data= get_batch(train_data_en,i,image_bool)
                 else : 
-                    # print("données FR")
-                    if image_bool : 
+                    train_data= get_batch(train_data_en,i)
+                model_A = model_en
+                model_B = model_fr
+            else : #FRENCH DATA
+                if image_bool : 
                         train_data= get_batch(train_data_fr,i,image_bool)
-                        
-                    else : 
-                        train_data= get_batch(train_data_fr,i)
-                    model_A = model_fr
-                    model_B = model_en
-                loss = cycle_consistency_train(model_A,model_B,train_data,image_bool)
-            else: #Auto encoding
-                
-                if np.random.rand()<1/2 : #English
-                    if image_bool : 
-                        train_data= get_batch(train_data_en,i,image_bool)
-                        
-                    else : 
-                        train_data= get_batch(train_data_en,i)
-                    model_A = model_en
-                else : #French
-                    if image_bool : 
-                        train_data= get_batch(train_data_fr,i,image_bool)
-                        
-                    else : 
-                        train_data= get_batch(train_data_fr,i)
-                    model_A = model_fr
+                else : 
+                    train_data= get_batch(train_data_fr,i)
+                model_A = model_fr
+                model_B = model_en
+            if V < 1 / 3 :#AUTO ENCODING
                 loss = auto_encoding_train(model_A,train_data,image_bool)
+            elif V < 2/3 :#CYCLE CONSISTENT
+                loss = cycle_consistency_train(model_A,model_B,train_data,image_bool)
+            else : #DIFFERENTIABLE CYCLE
+                loss = differentiable_cycle_consistency_train(model_A,model_B,train_data,image_bool)
             loss_list.append(loss)
             print(loss)
             total_loss+=loss
-            
-            
-            
             if (i%log_interval == 40 and i !=0) or i == N-1 : 
                 print("Iteration : " + str(i_iter) + " batch numéro : "+str(i)+" en "+ str(int(1000*(time.time()-start_time)/log_interval)) + " ms par itération, moyenne loss "+ str(total_loss/log_interval)) 
                 total_loss = 0

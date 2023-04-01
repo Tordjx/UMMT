6a2bd12cf46931cc71318fd4b1bcd78f7a0a5296
tordjx <valentin.tordjman--levavasseur@ensae.fr>
2023-04-01 09:45:17 +0000

    hyperparam changes

--- Core model files/Trainer.py
+++ Core model files/Trainer.py
@@ -223,6 +223,7 @@ def mixed_train(model_fr,model_en,train_data_fr,train_data_en,n_iter,batch_size,
                 model_B.loss_list.append(loss)
             else : #DIFFERENTIABLE CYCLE
                 loss = differentiable_cycle_consistency_train(model_A,model_B,train_data,image_bool)
+            
             loss_list.append(loss)
             # print(loss)
             total_loss+=loss

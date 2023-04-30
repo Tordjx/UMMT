5b5c82dcfa66d49a81bb33600f83421ab556eabc
Tordjx <valentin.tordjman--levavasseur@ensae.fr>
2023-04-30 19:00:12 +0200

    added eval at each epoch of blue

--- Core model files/evaluateur.py
+++ Core model files/evaluateur.py
@@ -1,9 +1,7 @@
-from nltk.translate.bleu_score import sentence_bleu 
+from nltk.translate.bleu_score import sentence_bleu , SmoothingFunction
 from nltk.translate.meteor_score import meteor_score
-from importlib import reload as _reload
-import greedy_beam_search
+from greedy_beam_search import *
 from Pipeline import *
-_reload(greedy_beam_search)
 import numpy as np
 
 def tensor_to_sentence(output,inv_dic):
@@ -47,14 +45,14 @@ def traduit(mode,model_A,model_B,src, inv_map_src,image_bool,tgt,inv_map_tgt,j):
     with open(model_A.prefix+"logs.txt",'a') as logs :
         if image_bool :
             if mode == 'greedy':
-                output = give_tokens(greedy_beam_search.CCF_greedy(model_A,model_B,data, features, True),model_B.padding_id,model_B.end_id)[j]
+                output = give_tokens(CCF_greedy(model_A,model_B,data, features, True),model_B.padding_id,model_B.end_id)[j]
             else : 
-                output = greedy_beam_search.CCF_beam_search(model_A, model_B, data, 3, features, True)[j]
+                output = CCF_beam_search(model_A, model_B, data, 3, features, True)[j]
         else :
             if mode == 'greedy':
-                output = give_tokens(greedy_beam_search.CCF_greedy(model_A,model_B,data, None, False),model_B.padding_id,model_B.end_id)[j]
+                output = give_tokens(CCF_greedy(model_A,model_B,data, None, False),model_B.padding_id,model_B.end_id)[j]
             else : 
-                output = greedy_beam_search.CCF_beam_search(model_A, model_B, data, 3, None, False)[j]
+                output = CCF_beam_search(model_A, model_B, data, 3, None, False)[j]
         logs.write("\nBleu score\n")
         bleu = evalue_bleu(output.view(-1),inv_map_tgt,tgt.view(-1))
         logs.write(str(bleu))
@@ -114,5 +112,90 @@ def evaluation(mode,val_data_en,val_data_fr,batch_size,model_en,model_fr,inv_map
         logs.close()
     return bleu, meteor
 
+import torchtext
+import pandas as pd
+def dataframe_eval(model_fr,model_en,val_data_en,val_data_fr,inv_map_en,inv_map_fr,image_bool,batch_size) :
+    model_fr.eval()
+    model_en.eval()
+    if image_bool :
+        val_data_en,features= val_data_en
+        val_data_fr,features= val_data_en
+    else :
+        val_data_en,val_data_fr = val_data_en,val_data_fr
+    batched_data_en,batched_data_fr=batchify([val_data_en,val_data_fr],batch_size,image_bool,conservative=True,permute = False)
+    if image_bool:
+        src,features = batched_data_en
+        tgt,_ = batched_data_fr
+    else :
+        src,tgt = batched_data_en,batched_data_fr
+    traductions_en_fr = []
+    references_en = []
+    traductions_fr_en = []
+    references_fr = []
+    for batch in range(len(src)):
+        print(batch)
+        if image_bool :
+            src[batch],features[batch],tgt[batch] = src[batch].to(device),features[batch].to(device),tgt[batch].to(device)
+            traduction = torch.argmax(CCF_greedy(model_en,model_fr,src[batch],features[batch],image_bool) ,dim = 2)
+        else :
+            src[batch] ,tgt[batch]= src[batch].to(device),tgt[batch].to(device)
+            traduction = torch.argmax(CCF_greedy(model_en,model_fr,src[batch],None,image_bool) ,dim = 2)
+        
+        for i in range(traduction.shape[0]):
+            traductions_en_fr.append([inv_map_fr[traduction[i][j].item()]  for j in range(traduction.shape[1]) if inv_map_fr[traduction[i][j].item()] not in ["TOKEN_VIDE","DEBUT_DE_PHRASE","FIN_DE_PHRASE"]])
+            references_fr.append([inv_map_fr[tgt[batch][i][j].item()] for j in range(tgt[batch].shape[1]) if inv_map_fr[tgt[batch][i][j].item()] not in ["TOKEN_VIDE","DEBUT_DE_PHRASE","FIN_DE_PHRASE"]])
+        if image_bool :
+            traduction = torch.argmax(CCF_greedy(model_fr,model_en,tgt[batch],features[batch],image_bool) ,dim = 2)
+        else :
+            traduction = torch.argmax(CCF_greedy(model_fr,model_en,tgt[batch],None,image_bool) ,dim = 2)
+        for i in range(traduction.shape[0]):
+            traductions_fr_en.append([inv_map_en[traduction[i][j].item()]  for j in range(traduction.shape[1]) if inv_map_en[traduction[i][j].item()] not in ["TOKEN_VIDE","DEBUT_DE_PHRASE","FIN_DE_PHRASE"]])
+            references_en.append([inv_map_en[src[batch][i][j].item()] for j in range(src[batch].shape[1]) if inv_map_en[src[batch][i][j].item()] not in ["TOKEN_VIDE","DEBUT_DE_PHRASE","FIN_DE_PHRASE"]])
+        if image_bool: #Si image, on rajoute les traductions sans images
+            traductions_en_fr_txt_only = []
+            traductions_fr_en_txt_only = []
+            src[batch],tgt[batch] = src[batch].to(device),tgt[batch].to(device)
+            traduction = torch.argmax(CCF_greedy(model_en,model_fr,src[batch],None,False) ,dim = 2)
+            for i in range(traduction.shape[0]):
+                traductions_en_fr_txt_only.append([inv_map_fr[traduction[i][j].item()]  for j in range(traduction.shape[1]) if inv_map_fr[traduction[i][j].item()] not in ["TOKEN_VIDE","DEBUT_DE_PHRASE","FIN_DE_PHRASE"]])
+                references_fr.append([inv_map_fr[tgt[batch][i][j].item()] for j in range(tgt[batch].shape[1]) if inv_map_fr[tgt[batch][i][j].item()] not in ["TOKEN_VIDE","DEBUT_DE_PHRASE","FIN_DE_PHRASE"]])
+            traduction = torch.argmax(CCF_greedy(model_fr,model_en,tgt[batch],None,False) ,dim = 2)
+            for i in range(traduction.shape[0]):
+                traductions_fr_en_txt_only.append([inv_map_en[traduction[i][j].item()]  for j in range(traduction.shape[1]) if inv_map_en[traduction[i][j].item()] not in ["TOKEN_VIDE","DEBUT_DE_PHRASE","FIN_DE_PHRASE"]])
+                references_en.append([inv_map_en[src[batch][i][j].item()] for j in range(src[batch].shape[1]) if inv_map_en[src[batch][i][j].item()] not in ["TOKEN_VIDE","DEBUT_DE_PHRASE","FIN_DE_PHRASE"]])
+            
+    if image_bool:
+        data = {"traductions_en_fr":traductions_en_fr,"references_fr":references_fr,"traductions_fr_en":traductions_fr_en,"references_en":references_en,"traductions_en_fr_txt_only":traductions_en_fr_txt_only,"traductions_fr_en_txt_only":traductions_fr_en_txt_only}
+    else :
+        data = {"traductions_en_fr":traductions_en_fr,"references_fr":references_fr,"traductions_fr_en":traductions_fr_en,"references_en":references_en}
+    
+    df = pd.DataFrame(data)
+    return df.loc()[:val_data_fr.shape[0]-1]
 
-#%%
+def bleu(row,langue_src):
+    if langue_src == "en":
+        candidates= list(row["traductions_en_fr"])
+        references = [list(row["references_fr"])]
+        return sentence_bleu(references, candidates,smoothing_function=SmoothingFunction().method4)
+    else : 
+        candidates= list(row["traductions_fr_en"])
+        references = [list(row["references_en"])]
+        return sentence_bleu(references, candidates,smoothing_function=SmoothingFunction().method4)
+def bleu_txt_only(row,langue_src):
+    if langue_src == "en":
+        candidates= list(row["traductions_en_fr_text_only"])
+        references = [list(row["references_fr"])]
+        return sentence_bleu(references, candidates,smoothing_function=SmoothingFunction().method4)
+    else : 
+        candidates= list(row["traductions_fr_en_text_only"])
+        references = [list(row["references_en"])]
+        return sentence_bleu(references, candidates,smoothing_function=SmoothingFunction().method4)
+    
+def save_dataframe_eval(model_fr,model_en,tokenized_val_en,tokenized_val_fr,inv_map_en,inv_map_fr,image_bool,batch_size,epoch = 0):
+    df = dataframe_eval(model_fr,model_en,tokenized_val_en,tokenized_val_fr,inv_map_en,inv_map_fr,image_bool,batch_size)
+    df["bleu_en_fr"] = df.apply(lambda row: bleu(row,"en"), axis=1)
+    df["bleu_fr_en"] = df.apply(lambda row: bleu(row,"fr"), axis=1)
+    if image_bool:
+        df["bleu_en_fr_txt_only"] = df.apply(lambda row: bleu_txt_only(row,"en"), axis=1)
+        df["bleu_fr_en_txt_only"] = df.apply(lambda row: bleu_txt_only(row,"fr"), axis=1)
+    df.to_csv(str(epoch)+model_fr.prefix+"_eval.csv")
